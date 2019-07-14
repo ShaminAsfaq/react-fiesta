09e35760e888f9a685a902d96f7f7c37ac7aed40
Shamin Asfaq <shamin.asfaq@gmail.com>
2019-07-14 21:28:58 +0600

    Commiting from germany using usm account from KMH's pc to connect to the local account name: shamin (from Deutschland)

--- Blog/src/actions/index.js
+++ Blog/src/actions/index.js
@@ -1,10 +1,22 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => {
     return async function(dispatch) {
         const response = await jsonPlaceholder.get('/posts');
-        dispatch({ type: 'FETCH_POSTS', payload: response })
+        dispatch({ type: 'FETCH_POSTS', payload: response.data })
     };
 };
+
+export const fetchUser = (id) => {
+    return function(dispatch) {
+        _fetchUser(id, dispatch);
+    }
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({ type: 'FETCH_USER', payload: response.data })
+})
 
 
