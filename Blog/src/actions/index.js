17e0079efc221a41bbcc87f05bb5f852a43b2e16
Shamin Asfaq <shamin.asfaq@gmail.com>
2019-07-15 12:14:02 +0600

    Project 'Blog' ended with so many questions in my mind. Will have to go through it again for better understanding of Redux

--- Blog/src/actions/index.js
+++ Blog/src/actions/index.js
@@ -1,6 +1,13 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    
+    var userIds = _.uniq(_.map(getState().posts, 'userId'))
+    userIds.forEach(id => dispatch(fetchUser(id)))
+}
+
 export const fetchPosts = () => {
     return async function(dispatch) {
         const response = await jsonPlaceholder.get('/posts');
@@ -8,7 +15,7 @@ export const fetchPosts = () => {
     };
 };
 
-const _fetchUser = async () => {
+export const fetchUser = id => async (dispatch) => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
     dispatch({ type: 'FETCH_USER', payload: response.data })
 }
