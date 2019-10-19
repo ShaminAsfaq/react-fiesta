54e2806c608a6e7f088766efd77a9bbb9cedf9b9
shamin <shamin.asfaq@gmail.com>
2019-10-20 00:15:55 +0600

    Weekend vibes :3

---
+++ indecision-app/src/playground/es6-classes-1.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(name = 'J. Doe') {
+        this.name = name;
+    }
+}
+
+const me = new Person('Shamin Asfaq');
+console.log(me);
+const casper = new Person('Shusoi');
+console.log(casper);
