1dd42a3b541370a1548f0a7e9cb9ae6f06824643
Shamin Asfaq <shamin.asfaq@gmail.com>
2019-10-23 17:15:08 +0600

    Finished till 6x08.

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -15,4 +15,9 @@
 // console.log('If 25 adult? -> ' + isAdult(25));
 // console.log('If 18 drinkeligible? -> ' + canDrink(18));
 
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const template = <p>Test</p>;
+
+ReactDOM.render(template, document.getElementById('app'));
