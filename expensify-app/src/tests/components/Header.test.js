30116eab88a734e1eb0b175cacca0b9278fe7df3
Shamin Asfaq <shamin.asfaq@gmail.com>
2019-11-24 09:21:42 +0600

    Mead 12x8

---
+++ expensify-app/src/tests/components/Header.test.js
@@ -0,0 +1,13 @@
+import ReactShallowRenderer from 'react-test-renderer/shallow';
+import React from 'react';
+import Header from '../../components/Header';
+
+test('Testing HeaderComponent', () => {
+    const renderer = new ReactShallowRenderer();
+    renderer.render(<Header />)
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    console.log(renderer.getRenderOutput())
+})
+
+
+
