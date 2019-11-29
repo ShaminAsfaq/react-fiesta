fff55b95df5841889effea8001695e8deddfec61
shamin <shamin.asfaq@gmail.com>
2019-11-30 01:03:35 +0600

    shamin-lives initialized, a bit front-end design.

---
+++ shamin-lives/src/components/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import Clock from './Clock';
+
+import Chronicle from './Chronicle';
+import Home from './Home';
+
+const Menu = () => {
+    return(
+        <div className="ui top fixed secondary pointing menu" style={{ backgroundColor: 'white' }}>
+            <NavLink to="/" exact={true} className="item">
+                Home
+            </NavLink>
+            <NavLink to="/new" className="item">
+                Create
+            </NavLink>
+            <div className="right menu">
+                <div className="ui item">
+                <Clock />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Menu />
+                <Switch>
+                    <Route path='/' component={Home} exact={true}/>
+                    <Route path='/new' component={Chronicle}/>
+                    <Route component={() => (<div>404 - <Link to="/">Go Home</Link></div>)}/>
+                </Switch>
+             </div>
+        </BrowserRouter>
+    );
+}
+
+export {
+    App as default
+}
