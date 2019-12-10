88a5398e3f5c5584f572248fca8073ad783a05ae
shamin <shamin.asfaq@gmail.com>
2019-12-11 03:52:24 +0600

    Minor Changes.

--- shamin-lives/shamin-lives-ui/src/components/Memento.js
+++ shamin-lives/shamin-lives-ui/src/components/Memento.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Memento = () => {
     return (
-            <div className="ui card">
-                <div className="content">
-                    <div className="header">Cute Dog</div>
-                    <div className="description">
+            <div className="ui card" style={{ cursor: 'pointer' }}>
+                <div className="content" style={{ backgroundColor: '#b7d7e8' }}>
+                    <div className="header" style={{ color: 'black' }}>Cute Dog</div>
+                    <div className="description" style={{ color: 'black' }}>
                     <p>Cute dogs come in a variety of shapes and sizes. Some cute dogs are cute for their adorable faces, others for their tiny stature, and even others for their massive size.</p>
     <p>Many people also have their own barometers for what makes a cute dog.</p>
                     </div>
                 </div>
-                <div className="extra content">
+                <div className="extra content" style={{ backgroundColor: '#87bdd8', color: 'white' }}>
                     <span className="left floated like">
                     <i className="like icon"></i>
                     Like
